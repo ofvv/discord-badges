06693f2c13690b89cbed110779c46b1228ae97d4
Niki <contact@zirobot.xyz>
2021-05-18 13:19:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,46 +11,46 @@ client.on("guildMemberAdd", async member => {
   let userflags = member.user.flags;
 if (userflags.has('PARTNERED_SERVER_OWNER')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.discordpartner)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('DISCORD_EMPLOYEE')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.discordstaff)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('HYPESQUAD_EVENTS')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.hypesquadevents)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('BUGHUNTER_LEVEL_1')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.bughunterlevel1)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('HOUSE_BRAVERY')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.hypesquadbravery)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('HOUSE_BRILLIANCE')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.hypesquadbrilliance)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('HOUSE_BALANCE')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.hypesquadbalance)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('EARLY_SUPPORTER')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.earlysupporter)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('BUGHUNTER_LEVEL_2')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.bughunterlevel2)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 }
 if (userflags.has('EARLY_VERIFIED_DEVELOPER')) {
 const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.botdeveloper)
-await member.roles.add(badgerole)
+await member.roles.add(badgerole).catch(() => {})
 } else if (userflags.has('VERIFIED_DEVELOPER')) {
   const badgerole = member.guild.roles.cache.find(r => r.id === config.roles.botdeveloper)
-  await member.roles.add(badgerole)
+  await member.roles.add(badgerole).catch(() => {})
 }
 } catch (e) {}
 })
